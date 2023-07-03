4142075d0b4c0ea1814fb569f1ae7c7d0625f94b
Brian <61602086+Nullfuse@users.noreply.github.com>
2023-07-03 00:31:02 -0700

    Update diagnostics.ts

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -5,63 +5,111 @@
 /** To demonstrate code actions associated with Diagnostics problems, this file provides a mock diagnostics entries. */
 
 import * as vscode from 'vscode';
+import * as path from 'path';
+// var path = require('path');
 
 /** Code that is used to associate diagnostic entries with code actions. */
-export const EMOJI_MENTION = 'emoji_mention';
+export const POSSIBLE_THREAD_DIVERGENCE = 'possible_thread_divergence';
 
-/** String to detect in the text document. */
-const EMOJI = 'emoji';
+let diagnosticMessage = new Map<string, string>([
+	["possible_thread_divergence", "This May Cause Thread Divergence"]
+]);
+
+function exec(cmd: string, handler = function(err: string, stdout: string, stderr: string){if(err || stderr){console.log(err + ' ' + stderr);return;}else{return stdout;}}) {
+	const childfork = require('child_process');
+	return childfork.execSync(cmd, handler);
+}
 
 /**
  * Analyzes the text document for problems. 
  * This demo diagnostic problem provider finds all mentions of 'emoji'.
  * @param doc text document to analyze
- * @param emojiDiagnostics diagnostic collection
+ * @param threadDivergenceDiagnostics diagnostic collection
  */
-export function refreshDiagnostics(doc: vscode.TextDocument, emojiDiagnostics: vscode.DiagnosticCollection): void {
+export function refreshDiagnostics(context: vscode.ExtensionContext, doc: vscode.TextDocument, threadDivergenceDiagnostics: vscode.DiagnosticCollection): void {
 	const diagnostics: vscode.Diagnostic[] = [];
 
-	for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
-		const lineOfText = doc.lineAt(lineIndex);
-		if (lineOfText.text.includes(EMOJI)) {
-			diagnostics.push(createDiagnostic(doc, lineOfText, lineIndex));
+	console.log(vscode.window.activeTextEditor?.document.uri.fsPath);
+
+	// 'py ' + '\"' + context.extensionPath + '\\src' + '\\Test copy.py' + '\"'
+	let cppcheckOutputString = exec('py' + ' ' + '\"' + path.join(context.extensionPath, 'src', 'Test.py') + '\"');
+	if (cppcheckOutputString !== undefined){
+		const cppcheckOutput = String(cppcheckOutputString).split(" ");
+		for (let i = 0; i < cppcheckOutput.length - 2; i+=2) {
+			console.log(cppcheckOutput[i] + ' ' + cppcheckOutput[i + 1] + '\n');
+			diagnostics.push(createDiagnostic(doc, cppcheckOutput[i + 1], doc.lineAt(parseInt(cppcheckOutput[i]) - 1), parseInt(cppcheckOutput[i]) - 1, 0, 0));
 		}
 	}
 
-	emojiDiagnostics.set(doc.uri, diagnostics);
+	/*
+	let cppcheckOutputString = exec('py ' + '\"' + context.extensionPath + '\\src' + '\\Test.py' + '\"');
+	if (cppcheckOutputString !== undefined){
+		const cppcheckOutput = String(cppcheckOutputString).split(" ");
+		for (let i = 0; i < cppcheckOutput.length - 4; i+=4) {
+			console.log(cppcheckOutput[i] + ' ' + cppcheckOutput[i + 1] + ' ' + cppcheckOutput[i + 2] + ' ' + cppcheckOutput[i + 3] + '\n');
+			diagnostics.push(createDiagnostic(doc, doc.lineAt(parseInt(cppcheckOutput[i]) - 1), parseInt(cppcheckOutput[i]) - 1, parseInt(cppcheckOutput[i + 2]) - 1, parseInt(cppcheckOutput[i + 3]) - 1));
+		}
+	}
+	*/
+
+	/*
+	const cp = require('child_process')
+	cp.exec('py ' + '\"' + context.extensionPath + '\\src' + '\\Test.py' + '\"', (err: string, stdout: string, stderr: string) => {
+		if (err) {
+			console.log('error: ' + err);
+		}
+		if (stdout) {
+			console.log(stdout);
+			const cppcheckOutput = stdout.split(" ");
+			for (let i = 0; i < cppcheckOutput.length - 4; i+=4) {
+				console.log(cppcheckOutput[i] + ' ' + cppcheckOutput[i + 1] + ' ' + cppcheckOutput[i + 2] + ' ' + cppcheckOutput[i + 3] + '\n');
+				diagnostics.push(createDiagnostic(doc, doc.lineAt(parseInt(cppcheckOutput[i]) - 1), parseInt(cppcheckOutput[i]) - 1, parseInt(cppcheckOutput[i + 2]), parseInt(cppcheckOutput[i + 3])));
+			}
+		}
+		if (stderr) {
+			console.log('stderr: ' + stderr);
+		}
+	});
+	*/
+
+	/*
+	const cppcheckOutput = ['10', 'threadIdx', '8', '14', '13', 'threadIdx', '8', '14', '16', 'threadIdx', '8', '14', '28', 'threadIdx', '9', '28', '28', 'threadIdx', '30', '35'];
+	for (let i = 0; i < cppcheckOutput.length - 4; i+=4) {
+		console.log(cppcheckOutput[i] + ' ' + cppcheckOutput[i + 1] + ' ' + cppcheckOutput[i + 2] + ' ' + cppcheckOutput[i + 3] + '\n');
+		diagnostics.push(createDiagnostic(doc, doc.lineAt(parseInt(cppcheckOutput[i]) - 1), parseInt(cppcheckOutput[i]) - 1, parseInt(cppcheckOutput[i + 2]) - 1, parseInt(cppcheckOutput[i + 3]) - 1));
+	}
+	*/
+
+	threadDivergenceDiagnostics.set(doc.uri, diagnostics);
 }
 
-function createDiagnostic(doc: vscode.TextDocument, lineOfText: vscode.TextLine, lineIndex: number): vscode.Diagnostic {
-	// find where in the line of that the 'emoji' is mentioned
-	const index = lineOfText.text.indexOf(EMOJI);
-
+function createDiagnostic(doc: vscode.TextDocument, diagnosticCode: string, lineOfText: vscode.TextLine, lineIndex: number, startColumnNumber: number, endColumnNumber: number): vscode.Diagnostic {
 	// create range that represents, where in the document the word is
-	const range = new vscode.Range(lineIndex, index, lineIndex, index + EMOJI.length);
+	// const range = new vscode.Range(lineIndex, startColumnNumber, lineIndex, endColumnNumber);
 
-	const diagnostic = new vscode.Diagnostic(range, "When you say 'emoji', do you want to find out more?",
-		vscode.DiagnosticSeverity.Information);
-	diagnostic.code = EMOJI_MENTION;
+	const diagnostic = new vscode.Diagnostic(lineOfText.range, diagnosticMessage.get(diagnosticCode) ?? '', vscode.DiagnosticSeverity.Warning);
+	diagnostic.code = diagnosticCode;
 	return diagnostic;
 }
 
-export function subscribeToDocumentChanges(context: vscode.ExtensionContext, emojiDiagnostics: vscode.DiagnosticCollection): void {
+export function subscribeToDocumentChanges(context: vscode.ExtensionContext, threadDivergenceDiagnostics: vscode.DiagnosticCollection): void {
 	if (vscode.window.activeTextEditor) {
-		refreshDiagnostics(vscode.window.activeTextEditor.document, emojiDiagnostics);
+		refreshDiagnostics(context, vscode.window.activeTextEditor.document, threadDivergenceDiagnostics);
 	}
 	context.subscriptions.push(
 		vscode.window.onDidChangeActiveTextEditor(editor => {
 			if (editor) {
-				refreshDiagnostics(editor.document, emojiDiagnostics);
+				refreshDiagnostics(context, editor.document, threadDivergenceDiagnostics);
 			}
 		})
 	);
 
 	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, emojiDiagnostics))
+		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(context, e.document, threadDivergenceDiagnostics))
 	);
 
 	context.subscriptions.push(
-		vscode.workspace.onDidCloseTextDocument(doc => emojiDiagnostics.delete(doc.uri))
+		vscode.workspace.onDidCloseTextDocument(doc => threadDivergenceDiagnostics.delete(doc.uri))
 	);
 
 }
